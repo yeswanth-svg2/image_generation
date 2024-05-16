d79f9d4e4d1bf94df826bd51d2e6b10f36c57023
yeswanth-svg <130365668+yeswanth-svg@users.noreply.github.com>
2024-05-16 11:01:59 +0530

    second

--- app.py
+++ app.py
@@ -1,61 +1,55 @@
-from flask import Flask , render_template , request
+from flask import Flask, render_template, request, send_from_directory
 from gradio_client import Client
 import os
 import shutil
+import re
 
 app = Flask(__name__)
 
 client = Client("KingNish/Instant-Image")
 
 def generate_images(prompt):
-      
-    image=  client.predict(
-                prompt=prompt,
-                negative_prompt=prompt,
-                style="(No style)",
-                use_negative_prompt=False,
-                seed=0,
-                width=1024,
-                height=1024,
-                inference_steps=4,
-                randomize_seed=True,
-                api_name="/run"
-        )
+    image = client.predict(
+        prompt=prompt,
+        negative_prompt=prompt,
+        style="(No style)",
+        use_negative_prompt=False,
+        seed=0,
+        width=1024,
+        height=1024,
+        inference_steps=4,
+        randomize_seed=True,
+        api_name="/run"
+    )
     return image
-      
 
-@app.route('/' , methods=['GET' , 'POST'])
+def sanitize_filename(prompt):
+    # Remove any non-alphanumeric characters (except for spaces)
+    filename = re.sub(r'[^a-zA-Z0-9 ]', '', prompt)
+    # Replace spaces with underscores
+    filename = filename.replace(' ', '_')
+    # Truncate to 15 characters
+    return filename[:15]
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-      prompt= request.form['prompt']
-     
-      image= generate_images(prompt)
-    #   file_path = os.path.join('./static' , 'static.jpeg')
-      path=image[0][0]['image']
-      print(image)
-    #   path.save(file_path)
-      
-    #   print(path)
-      
-      source_destination = path
-      destination_path = './static/static.png'
+        prompt = request.form['prompt']
+        image = generate_images(prompt)
+        path = image[0][0]['image']
 
+        filename = sanitize_filename(prompt) + '.png'
+        destination_path = os.path.join('static', filename)
 
-      shutil.copy2(source_destination, destination_path)
-      print("image is uploaded to static directory")
+        shutil.copy2(path, destination_path)
 
-    
+        return render_template('index.html', image_filename=filename)
 
+    return render_template('index.html', image_filename=None)
 
-      return render_template('index.html' , image=image)
-    
-    return render_template('index.html')
-
-
-
-
-
-
+@app.route('/static/<filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' , debug=True)
+    app.run(host='0.0.0.0', debug=True)
